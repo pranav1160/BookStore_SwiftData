130d59288847413b008c7f2ca5ead49480e88231
Pranav <pranav1160ly@gmail.com>
2025-07-19 22:02:31 +0530

    Initial Commit

---
+++ BookStore/BookStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookStoreApp.swift
+//  BookStore
+//
+//  Created by Pranav on 19/07/25.
+//
+
+import SwiftUI
+
+@main
+struct BookStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
